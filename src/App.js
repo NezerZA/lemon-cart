01e833e664c7b5f401a9a162d4bda82468b52e80
Eben Coetzer <eben@aves.co.za>
2023-09-20 22:17:26 +0200

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  // const [items, setItems] = useState([]);
+  const Items = [{ id: 1, itemName: "apple", quantity: 6 }];
+
+  // const initialItems = [
+  //   { id: 1, description: "Passports", quantity: 2, packed: false },
+  //   { id: 2, description: "Socks", quantity: 12, packed: false },
+  // ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Insert />
+      <List />
+      <Footer />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return (
+    <div className="header">
+      <h1>
+        <strong>Lemon</strong>Cart 🍋
+      </h1>
+    </div>
+  );
+}
+
+function Insert() {
+  return (
+    <form className="add-form">
+      <input placeholder="Item..."></input>
+      <input placeholder="Quantity..."></input>
+      <button>Add</button>
+    </form>
+  );
+}
+
+function List() {
+  return (
+    <ul className="list">
+      <ListItem />
+    </ul>
+  );
+}
+
+function ListItem() {
+  return <li className="item">Item</li>;
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <h3>Enjoy! Made by Eben Coetzer with React</h3>
+    </div>
+  );
+}
